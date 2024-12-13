26db5110b5d969216ed19b32bd7a55ab41f5f501
Rahul Mijar <rahulmijar0243@gmail.com>
2024-12-13 18:40:27 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>HelloWorld</div>;
+};
+
+export default App;
---
+++ src/ui/shared/Heading.tsx
@@ -0,0 +1,9 @@
+const Heading = () => {
+  return (
+    <div className="p-2">
+      <h1>CODEPEN</h1>
+    </div>
+  );
+};
+
+export default Heading;
